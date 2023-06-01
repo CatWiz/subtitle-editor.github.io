8d83032d40e299da0ac89f1dc62f177b3e2fd0e3
CatWizard <thecatwizard124@gmail.com>
2023-06-01 14:34:47 +0300

    readded video prop

--- pages/components/VideoPlayer.tsx
+++ pages/components/VideoPlayer.tsx
@@ -1,6 +1,12 @@
+import SubtitleEntry from '../Classes/SubtitleEntry'
 import styles from '../index_styles.module.css'
 
-export default function VideoPlayer({id}: {id: string}) {
+export default function VideoPlayer(
+    {id, subtitles}: {
+        id: string,
+        subtitles: SubtitleEntry[]
+    }
+) {
     return (
         <video id={id} controls className={styles['video']}></video>
     )
