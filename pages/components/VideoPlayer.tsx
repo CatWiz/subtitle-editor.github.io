b6f3f5d90fab17bd9d944ae043a83088beb733ee
CatWizard <thecatwizard124@gmail.com>
2023-06-01 14:20:07 +0300

    now with desync errors!

--- pages/components/VideoPlayer.tsx
+++ pages/components/VideoPlayer.tsx
@@ -1,7 +1,23 @@
+import SubtitleEntry from '../Classes/SubtitleEntry'
 import styles from '../index_styles.module.css'
 
-export default function VideoPlayer({id}: {id: string}) {
+export default function VideoPlayer(
+    {id, subtitles}: {
+        id: string,
+        subtitles: SubtitleEntry[],
+    },
+
+) {
+    const subVTT = 'WEBVTT\n\n' + subtitles.map(sub => sub.ToVTT()).join('\n\n')
+    const VTTFile = new Blob([subVTT], {type: 'text/plain'})
+    const url = URL.createObjectURL(VTTFile)
+
+    console.log(VTTFile.text())
+
     return (
-        <video id={id} controls className={styles['video']}></video>
+        <video id={id} controls className={styles['video']}>
+            <track kind="captions" srcLang="en" label="English" src={url} default/>
+
+        </video>
     )
 }
